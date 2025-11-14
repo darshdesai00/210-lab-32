583152572eac406702af8e92a8ea3fe4135286cc
darshdesai00 <darshdesai50@icloud.com>
2025-11-14 14:43:39 -0800

    adding program structure

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>   // for cout
+#include <deque>      // required for std::deque
+#include "Car.h"      // Car class provided by instructor
+
+using namespace std;
+
+const int INITIAL_SIZE = 2
+
+int main(){
+    cout << "Begining The Toll Booth Simulation\n";
+
+    deque<
+
+}
+
+
