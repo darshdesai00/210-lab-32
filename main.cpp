3aab671d1d6ccdb8fa7f31e468c7db797e496ab4
darshdesai00 <darshdesai50@icloud.com>
2025-11-14 14:56:05 -0800

    Fixing errors for Milestone 1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>   // for cout
 #include <deque>      // required for std::deque
 #include "Car.h"      // Car class provided by instructor
-
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 const int INITIAL_SIZE = 2
@@ -9,8 +10,29 @@ const int INITIAL_SIZE = 2
 int main(){
     cout << "Begining The Toll Booth Simulation\n";
 
-    deque<
+    deque<Car> lane; // added a makeshift lane for the toll
 
-}
+    for (int i - 0; i < INITIAL_SIZE;) { // added INITIAL_SIZE cars
+        lane.pushback(Car());
+
+    }
+
+    // print initial queue
+    cout << "Initial queue:\n";
+    for (auto &c : lane) {
+        cout << "    ";
+        c.print();     
+    }
+
+    cout << "Front car: " ; // access front and back
+    lane.front().print();
+    
+    cout << "Back car: ";
+    lane.back().print
+
+    return 0;
+}    
+
+
 
 
