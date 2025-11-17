83cbc2bf764bb29498594e8faf491e95aba28ac2
darshdesai00 <darshdesai50@icloud.com>
2025-11-16 19:49:55 -0800

    Finishing up milestone 3 and fixing errors

--- main.cpp
+++ main.cpp
@@ -8,66 +8,21 @@
 using namespace std;
 
 const int NUM_LANES = 4;
+const int INITIAL_SIZE = 2;
 
 int main(){
 
-    cout << "Testing multi-lane plaza structure\n\n";
+    cout << "Initial queue:\n";
 
     deque<Car> lanes[NUM_LANES]; // created a array 4 lanes
 
-    cout << "Adding test cars to each lane...\n";
-    for (int i = 0; i < NUM_LANES; i++) { // added INITIAL_SIZE cars
-        lanes[i].push_back(Car()); // pushes a test car
-    }
-
-    // print data of each line
-    cout << "Current Line Status:\n";
+    // filled each lane with 2 starting cars
     for (int i = 0; i < NUM_LANES; i++) {
-        cout << "Lane " << (i + 1) << ":\n";
-        for (auto &c : lanes[i]) {
-            cout << "    ";
-            c.print();    
-        }
+    for (int j = 0; j < INITIAL_SIZE; j++) {
+    lanes[i].push_back(Car());
+    }
     }
 
-    // Test accessing front and back of lane 1
-    cout << "\nTesting the front/back of Lane 1:\n";; 
-    lanes[0].front().print();
-    lanes[0].back().print();
-    
-    // add another car to lane 2
-    cout << "\nAdding a second car to Lane 2\n";
-    lanes[1].push_back(Car());
-
-    cout << "Lane 2 now has:\n";
-    for (auto &c : lanes[1]) {
-        cout << "    ";
-        c.print();
-    }
-    
-
-    // Remove the front car from Lane 3
-    cout << "\nRemoving front car from Lane 3...\n";
-    if (!lanes[2].empty()) {
-    Car leaving = lanes[2].front();
-    lanes[2].pop_front();
-    cout << "Removed: ";
-    leaving.print();
-    }
-    
-    // The final lane status
-    cout << "final lane status:\n";
-    cout << "\nFinal lane structure after tests:\n";
+    // print all lanes in correct format
     for (int i = 0; i < NUM_LANES; i++) {
-    cout << "Lane " << (i + 1) << ":\n";
-    if (lanes[i].empty()) {
-    cout << "    (empty)\n";
-    } else {
-    for (auto &c : lanes[i]) {
-    cout << "    ";
-    c.print();
-            }
-        }   
-    }
-return 0; 
-}
+    
